5527620050840d95871405e33248b46da7546674
You Jeong Jang <sunny819819@naver.com>
2024-01-01 16:15:42 +0900

    ✨initial commit

---
+++ combus-server/src/main/java/gdsc/combusserver/CombusServerApplication.java
@@ -0,0 +1,13 @@
+package gdsc.combusserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombusServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombusServerApplication.class, args);
+	}
+
+}
---
+++ combus-server/src/test/java/gdsc/combusserver/CombusServerApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.combusserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombusServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
