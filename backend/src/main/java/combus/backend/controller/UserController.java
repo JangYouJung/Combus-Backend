be8c6a1d040425ad690122fe3504d9957e17fcd8
You Jeong Jang <sunny819819@naver.com>
2024-01-06 02:11:34 +0900

    [feat] 사용자 로그인 세션 구현

--- backend/src/main/java/combus/backend/controller/UserController.java
+++ backend/src/main/java/combus/backend/controller/UserController.java
@@ -4,10 +4,13 @@ import combus.backend.domain.User;
 import combus.backend.repository.UserRepository;
 import combus.backend.request.LoginRequest;
 import combus.backend.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,18 +32,33 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest LoginRequest) {
+    public String login(@RequestBody LoginRequest LoginRequest, BindingResult bindingResult,
+                                   HttpServletRequest httpServletRequest) {
 
         String loginId = LoginRequest.getLoginId();
         System.out.println("loginId: "+ loginId);
 
-        User authenticatedUser = userService.authenticateUser(loginId);
+        User loginUser = userService.authenticateUser(loginId);
 
-        if (authenticatedUser != null) {
-            // Return user information or a token in the response
-            return ResponseEntity.ok(authenticatedUser);
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("회원 번호가 올바르지 않습니다.");
+        // 회원 번호가 틀린 경우
+        if(loginUser == null){
+            bindingResult.reject("login failed", "로그인 실패! 회원 번호를 확인해주세요.");
         }
+        if(bindingResult.hasErrors()) {
+            return "redirect:/users/login";
+        }
+
+
+        // 로그인 성공 => 세션 생성
+        // 세션을 생성하기 전에 기존의 세션 파기
+        httpServletRequest.getSession().invalidate();
+        HttpSession session = httpServletRequest.getSession(true);  // Session이 없으면 생성
+
+        // 세션에 user의 기본키 Id를 넣어줌
+        session.setAttribute("userId", loginUser.getId());
+        session.setMaxInactiveInterval(7200); // Session이 2시간동안 유지
+
+        return "home";
     }
+
 }
