90a63070f94b0ca4b155ab68a626742ace19a4c9
You Jeong Jang <sunny819819@naver.com>
2024-01-06 02:33:49 +0900

    [feat] 버스 기사 로그인 & 세션 구현

---
+++ backend/src/main/java/combus/backend/repository/DriverRepository.java
@@ -0,0 +1,12 @@
+package combus.backend.repository;
+
+import combus.backend.domain.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, String> {
+    Optional<Driver> findByLoginId(String loginId);
+}
