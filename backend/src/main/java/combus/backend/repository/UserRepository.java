0eaa05a41887494a9d42e21d23f916861469b084
You Jeong Jang <sunny819819@naver.com>
2024-01-06 01:44:51 +0900

    [feat] 사용자 로그인 구현

---
+++ backend/src/main/java/combus/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package combus.backend.repository;
+import combus.backend.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByLoginId(String loginId);
+}
