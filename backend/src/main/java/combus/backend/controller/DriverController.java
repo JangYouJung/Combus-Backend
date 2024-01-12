1266bd4669fcb20f847edb0d3a299bd5903df6c9
You Jeong Jang <sunny819819@naver.com>
2024-01-13 00:41:14 +0900

    (feat)버스기사 로그인 수정
    로그인 성공시 버스기사 정보를 리턴하도록 수정

--- backend/src/main/java/combus/backend/controller/DriverController.java
+++ backend/src/main/java/combus/backend/controller/DriverController.java
@@ -1,12 +1,16 @@
 package combus.backend.controller;
 
 import combus.backend.domain.Driver;
+import combus.backend.dto.LoginDriverResponseDto;
 import combus.backend.repository.DriverRepository;
 import combus.backend.request.LoginRequest;
 import combus.backend.service.DriverService;
+import combus.backend.util.ResponseCode;
+import combus.backend.util.ResponseData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,22 +32,23 @@ public class DriverController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest LoginRequest, BindingResult bindingResult,
-                        HttpServletRequest httpServletRequest) {
+    public ResponseEntity<ResponseData<LoginDriverResponseDto>> login(@RequestBody LoginRequest LoginRequest, BindingResult bindingResult,
+                                                                      HttpServletRequest httpServletRequest) {
 
         String loginId = LoginRequest.getLoginId();
         System.out.println("loginId: "+ loginId);
 
-        Driver loginDriver = driverService.authenticateDriver(loginId);
+        Driver driver = driverService.authenticateDriver(loginId);
 
         // 회원 번호가 틀린 경우
-        if(loginDriver == null){
+        if(driver == null){
             bindingResult.reject("login failed", "로그인 실패! 회원 번호를 확인해주세요.");
         }
         if(bindingResult.hasErrors()) {
-            return "redirect:/users/login";
+            return ResponseData.toResponseEntity(ResponseCode.ACCOUNT_NOT_FOUND,null);
         }
 
+        LoginDriverResponseDto loginDriver = new LoginDriverResponseDto(driver);
 
         // 로그인 성공 => 세션 생성
         // 세션을 생성하기 전에 기존의 세션 파기
@@ -51,10 +56,10 @@ public class DriverController {
         HttpSession session = httpServletRequest.getSession(true);  // Session이 없으면 생성
 
         // 세션에 user의 기본키 Id를 넣어줌
-        session.setAttribute("userId", loginDriver.getId());
+        session.setAttribute("userId", driver.getId());
         session.setMaxInactiveInterval(7200); // Session이 2시간동안 유지
 
-        return "home";
+        return ResponseData.toResponseEntity(ResponseCode.SIGNIN_SUCCESS,loginDriver);
     }
 
 
---
+++ backend/src/main/java/combus/backend/domain/BusMatch.java
@@ -0,0 +1,22 @@
+package combus.backend.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BusMatch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long driverId;
+    private Long busId;
+}
