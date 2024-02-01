3dddc6e2597297aa1e788362dc44ce10203861ad
You Jung <80906691+JangYouJung@users.noreply.github.com>
2024-02-02 01:06:54 +0900

    feat: 버스 정류장 이름으로 예약하기 구현

--- backend/src/main/java/combus/backend/controller/ReservationController.java
+++ backend/src/main/java/combus/backend/controller/ReservationController.java
@@ -36,9 +36,10 @@ public class ReservationController {
     @Autowired
     ReservationRepository reservationRepository;
 
-    @PostMapping("/")
-    public ResponseEntity<ResponseData> createReservation(@RequestBody ReservationRequest reservationRequest,
-                                                                                    @SessionAttribute(name = "userId", required = false)Long userId){
+    @PostMapping("/")   // 정류장 고유 번호로 예약하기
+    public ResponseEntity<ResponseData> createReservation(
+            @RequestBody ReservationRequest reservationRequest,
+            @SessionAttribute(name = "userId", required = false)Long userId){
 
         System.out.println("로그인한 유저ID: " + userId);
 
@@ -51,10 +52,10 @@ public class ReservationController {
             return new ResponseEntity<>(headers, HttpStatus.UNAUTHORIZED);
         }
 
-        Long boardingStopId = Long.parseLong(reservationRequest.getBoardingStop());   // 승차 정류소 번호 arsId
-        Long dropStopId = Long.parseLong(reservationRequest.getDropStop());           // 하파 정류소 번호 arsId
-        Long vehId = Long.parseLong(reservationRequest.getVehId());                 // 버스 고유 ID (같은 노선의 버스라도 다 다른 고유값)
-        String busRouteName = reservationRequest.getBusRouteNm();                  // 버스 노선명 (ex. 140번 버스)
+        String boardingStopId = reservationRequest.getBoardingStop();   // 승차 정류소 번호 arsId
+        String dropStopId = reservationRequest.getDropStop();           // 하파 정류소 번호 arsId
+        Long vehId = Long.parseLong(reservationRequest.getVehId());     // 버스 고유 ID (같은 노선의 버스라도 다 다른 고유값)
+        String busRouteName = reservationRequest.getBusRouteNm();       // 버스 노선명 (ex. 140번 버스)
 
         try {
             User user = userService.findUserById(userId);
@@ -83,6 +84,66 @@ public class ReservationController {
             e.printStackTrace(); // 예외 정보를 콘솔에 출력
             return ResponseData.toResponseEntity(ResponseCode.FAILED_RESERVATION_CREATTION);
         }
+    }
+
+    @PostMapping("/stt")   // 정류장 이름으로 예약하기
+    public ResponseEntity<ResponseData> createReservationByStName(
+            @RequestBody ReservationRequest reservationRequest,
+            @SessionAttribute(name = "userId", required = false)Long userId) {
+
+        /************************************************
+         *         시각 장애인이 음성으로 예약하는 경우          *
+         *    버스 정류소 id가 아닌 정류소명으로 예약해야 하므로    *
+         *        해당 기능을 위한 api를 따로 제작했다.         *
+        *************************************************/
+
+        System.out.println("로그인한 유저ID: " + userId);
+
+        // 세션이 끊어진 경우 -> 로그인 화면으로 redirect
+        if(userId == null){
+            System.out.println("세션 expired");
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(URI.create("/users/login"));
+            return new ResponseEntity<>(headers, HttpStatus.UNAUTHORIZED);
+        }
+
+        String boardingStopName = reservationRequest.getBoardingStop();         // 승차 정류소 이름
+        String dropStopName = reservationRequest.getDropStop();                 // 하차 정류소 이름
+        Long vehId = Long.parseLong(reservationRequest.getVehId());         // 버스 고유 ID (같은 노선의 버스라도 다 다른 고유값)
+        String busRouteName = reservationRequest.getBusRouteNm();           // 버스 노선명 (ex. 140번 버스)
+
+        try {
+            User user = userService.findUserById(userId);
+            if (user == null) {
+                throw new IllegalArgumentException("User with ID " + userId + " not found.");
+            }
+
+            BusStop boardingStop = busStopService.findByName(boardingStopName);
+            if (boardingStop == null) {
+                throw new IllegalArgumentException("Boarding stop with Name " + boardingStopName + " not found.");
+            }
+
+            BusStop dropStop = busStopService.findByName(dropStopName);
+            if (dropStop == null) {
+                throw new IllegalArgumentException("Drop stop with Name" + dropStopName + " not found.");
+            }
+
+            // 객체들이 유효한 경우
+            Reservation reservation = new Reservation(user,vehId,boardingStop,dropStop,busRouteName);
+            System.out.println(reservation.toString());
+            reservationRepository.save(reservation);
+
+            return ResponseData.toResponseEntity(ResponseCode.CREATE_RESERVATION_SUCCESS);
+
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace(); // 예외 정보를 콘솔에 출력
+            return ResponseData.toResponseEntity(ResponseCode.FAILED_RESERVATION_CREATTION);
+        }
+
+
+
+
     }
 
 
