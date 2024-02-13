cf694e9b2d3d8ce6e81a76317928ff53e9071d66
You Jung <80906691+JangYouJung@users.noreply.github.com>
2024-02-13 16:34:55 +0900

    [Refactor] 버스 기사: 홈화면 시각 장애인 탑승 여부 정보 추가

--- backend/src/main/java/combus/backend/dto/DriverHomeBusStopDto.java
+++ backend/src/main/java/combus/backend/dto/DriverHomeBusStopDto.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor
 public class DriverHomeBusStopDto {
-    private String arsId;        // 버스 정류장 고유 번호
+    private String arsId;       // 버스 정류장 고유 번호
     private String name;       // 버스 정류장 이름
     private double gpsX;       // 정류소 위치 x좌표 : 경도
     private double gpsY;       // 정류소 위치  y좌표 : 위도
@@ -20,7 +20,8 @@ public class DriverHomeBusStopDto {
     private int reserved_cnt;  // 승차 예약 인원수
     private int drop_cnt;      // 하차 예정 인원수
 
-    private boolean wheelchair;// 휠체어 사용객 탑승 여부
+    private boolean wheelchair; // 휠체어 사용객 탑승 여부
+    private boolean blind;      // 시각 장애인 승객 탑승 여부
 
 
 }
